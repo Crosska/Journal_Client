c4695f26737577e48ac2f88ca310cb9d719cba95
uncleandru <ebanas@bk.ru>
2019-07-16 08:36:05 +0300

    qwerty

--- Journal_Client/Form1.cs
+++ Journal_Client/Form1.cs
@@ -16,5 +16,10 @@ namespace Journal_Client
         {
             InitializeComponent();
         }
+
+        private void ProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
