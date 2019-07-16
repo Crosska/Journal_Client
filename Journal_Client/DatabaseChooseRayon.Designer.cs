e904256820cd6e54bcacc2dfac13e9ba2bb5ea4e
uncleandru <ebanas@bk.ru>
2019-07-16 11:56:47 +0300

    форма выбора района

--- Journal_Client/DatabaseChooseRayon.Designer.cs
+++ Journal_Client/DatabaseChooseRayon.Designer.cs
@@ -29,31 +29,58 @@
         private void InitializeComponent()
         {
             this.combocox_rayon = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_ok = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // combocox_rayon
             // 
             this.combocox_rayon.FormattingEnabled = true;
-            this.combocox_rayon.Location = new System.Drawing.Point(264, 116);
+            this.combocox_rayon.Location = new System.Drawing.Point(185, 39);
             this.combocox_rayon.Name = "combocox_rayon";
             this.combocox_rayon.Size = new System.Drawing.Size(121, 21);
             this.combocox_rayon.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(51, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выбор района";
+            // 
+            // button_ok
+            // 
+            this.button_ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_ok.Location = new System.Drawing.Point(138, 92);
+            this.button_ok.Name = "button_ok";
+            this.button_ok.Size = new System.Drawing.Size(88, 31);
+            this.button_ok.TabIndex = 2;
+            this.button_ok.Text = "OK";
+            this.button_ok.UseVisualStyleBackColor = true;
             // 
             // DatabaseChooseRayon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(357, 151);
+            this.Controls.Add(this.button_ok);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.combocox_rayon);
             this.Name = "DatabaseChooseRayon";
             this.Text = "Выбор района";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CloseAll);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ComboBox combocox_rayon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_ok;
     }
 }
