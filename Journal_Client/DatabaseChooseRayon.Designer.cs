895e93f65b667a7ececf26d7116dd3fe25c131ce
Crosska <musical.vg@gmail.com>
2019-07-16 14:10:31 +0300

    Общие улучшения

--- Journal_Client/DatabaseChooseRayon.Designer.cs
+++ Journal_Client/DatabaseChooseRayon.Designer.cs
@@ -46,17 +46,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(27, 13);
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(21, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(228, 24);
+            this.label1.Size = new System.Drawing.Size(243, 22);
             this.label1.TabIndex = 1;
             this.label1.Text = "Выберите район города";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button_ok
             // 
-            this.button_ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button_ok.Location = new System.Drawing.Point(17, 105);
             this.button_ok.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button_ok.Name = "button_ok";
